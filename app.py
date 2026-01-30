d253b0b72ca71459cae8a1e0dfd5e02edff7c3d9
Jakaria030 <gulamjakaria99@gmail.com>
2026-01-30 17:24:56 +0600

    Add SQLAlchemy and PostgreSQL database

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
+# ================== POSTGRESQL Connection ================== 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/taskdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# ================== Models ==================
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(50), nullable=False, default='todo')
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    due_date = db.Column(db.Date)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "status": self.status,
+            "created_at": self.created_at.isoformat(),
+            "due_date": self.due_date.isoformat() if self.due_date else None
+        }
+    
+# ================== Page Render ================== 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/tasks.html")
 def tasks():
-    return render_template("tasks.html")
+    return render_template('tasks.html')
+
+
+# ================== Program Start ================== 
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
