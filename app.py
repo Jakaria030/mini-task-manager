4e745593081732b07bffb40c19407286120a6443
Jakaria030 <gulamjakaria99@gmail.com>
2026-01-30 15:17:56 +0600

    Add home page design

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/tasks.html")
+def tasks():
+    return render_template("tasks.html")
